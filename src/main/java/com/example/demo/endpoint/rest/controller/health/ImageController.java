b259ff3fae133082d4da354c60ffe880ea7c0446
Nomena4 <hei.nomena.4@gmail.com>
2026-07-13 09:49:48 +0300

    refactor: move health controller to dedicated health subpackage

--- src/main/java/com/example/demo/endpoint/rest/controller/health/ImageController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/health/ImageController.java
@@ -1,4 +1,4 @@
-package com.example.demo.endpoint.rest.controller;
+package com.example.demo.endpoint.rest.controller.health;
 
 import com.example.demo.entity.ImageSubmission;
 import com.example.demo.service.ImageService;
